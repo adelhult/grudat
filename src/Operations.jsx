d3cfaed51974274b2bc0ecc794bd9e0e487266fa
Eli Adelhult <jxz2000@hotmail.se>
2021-01-03 21:42:46 +0100

    fix: search ignores a possible leading 0

--- src/Operations.jsx
+++ src/Operations.jsx
@@ -94,7 +94,7 @@ export default function Operations(props) {
       try {
         return ((op.shortHand.toUpperCase().search(query) >= 0) ||
           (op.type.toUpperCase().search(query) >= 0) ||
-          (op.code.toString().toUpperCase().search(query) >= 0));
+          (op.code.toString().toUpperCase().search(`0?${query}`) >= 0));
       } catch (err) {
         // if the regex breaks:
         setQuery(query.slice(0, -1));
